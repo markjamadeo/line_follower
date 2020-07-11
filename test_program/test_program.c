e09b0853d8d99b0a14b127d07ccab5f8e92fd940
Mark John <markjamadeo@gmail.com>
2020-07-11 16:50:44 +0800

    Initial Commit
    This will add test programs for the repo.

---
+++ test_program/test_program.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Welcome to MJ Raspi!\n");
+	printf("Let's create something great!\n");
+	return 0;
+}
---
+++ test_program/test_program.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"Welcome to MJ Raspi!"<<endl;
+	cout<<"Let's create something great!"<<endl;
+}
